c5193e080d40e5db9ef0f820c4a4e904124aaa71
lone-DO <citizin@outlook.com>
2025-08-25 13:31:29 +0200

    Remove Rocket Banner

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,23 +2,12 @@ import { NgModule } from '@angular/core';
 import { CounterControlsComponent } from './components/counter-controls/counter-controls.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { RocketBannerComponent } from './components/rocket-banner/rocket-banner.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    CounterControlsComponent,
-    FooterComponent,
-    HeaderComponent,
-    RocketBannerComponent,
-  ],
+  declarations: [CounterControlsComponent, FooterComponent, HeaderComponent],
   imports: [CommonModule, RouterModule],
-  exports: [
-    CounterControlsComponent,
-    FooterComponent,
-    HeaderComponent,
-    RocketBannerComponent,
-  ],
+  exports: [CounterControlsComponent, FooterComponent, HeaderComponent],
 })
 export class SharedModule {}
